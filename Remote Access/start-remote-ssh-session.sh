1b9016602092dfb4f7c3d0ab72ac9d19a42f3385
Allen Hancock <hancock@yesthatallen.com>
2014-07-07 21:21:19 -0500

    output file, more variables

--- Remote Access/start-remote-ssh-session.sh
+++ Remote Access/start-remote-ssh-session.sh
@@ -3,8 +3,8 @@ set -e
 
 function cleanup {
     #delete key
-	ssh-agent -D ~/wmpublic.key
-	rm -f ~/wmpublic.key
+	ssh-agent -D ~/wmsupport.pub
+	rm -f ~/wmsupport.pub
 	rm -f /etc/sshd_config.bak
 	sed -e 's/GatewayPorts yes #wm/#GatewayPorts no/'
 }
@@ -12,8 +12,8 @@ trap cleanup EXIT
 
 sed -i.bak 's/#GatewayPorts no/GatewayPorts yes #wm/g' /etc/sshd_config
 #copy down key
-curl https://support.watchmanmonitoring.com/wmsupport.pub -O ~/.ssh/wmsupport.pub
-ssh-add ~/wmpublic.key 
-ssh -R 4040:localhost:22 user-with-key@intermediary-ip
-echo "Key is added to the system and will be avaliable until this script is quit nicely"
+curl https://support.watchmanmonitoring.com/wmsupport.pub -o ~/wmsupport.pub
+ssh-add ~/wmsupport.pub
+ssh -R 4040:localhost:22 support@support.watchmanmonitoring.com
+echo "Key is added to the system and will be available until this script is quit gracefully"
 read
