e99fadac3a2e355eb54577c25a682dc40ebbf710
Allen Hancock <hancock@yesthatallen.com>
2014-09-01 23:44:47 -0500

    secure the key

--- Remote Access/start-remote-ssh-session.sh
+++ Remote Access/start-remote-ssh-session.sh
@@ -13,6 +13,7 @@ trap cleanup EXIT
 sed -i.bak 's/#GatewayPorts no/GatewayPorts yes #wm/g' /etc/sshd_config
 #copy down key
 curl https://support.watchmanmonitoring.com/wmsupport.pub -o ~/wmsupport.pub
+chmod 600 ~/wmsupport.pub
 ssh-add ~/wmsupport.pub
 ssh -R 4040:localhost:22 support@support.watchmanmonitoring.com
 echo "Key is added to the system and will be available until this script is quit gracefully"
